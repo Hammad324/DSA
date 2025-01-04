e5b582f598794fd5e845c8346453d66bd1f305b8
Hammad Farrukh <hammadfarrukh602@gmail.com>
2025-01-04 19:39:50 +0500

    updated

---
+++ Algorithms/Sorting/bubbleSort.js
@@ -0,0 +1,31 @@
+let arr = [4, 1, 5 ,2 ,3] // n = 5
+
+// sort the following array in ascending order
+
+const bubbleSort = (arr) => {
+    for (let i = 0; i < arr.length - 1; i++) { // The outer loop runs n - 1 times because we don’t need to check the last element in the n-th pass; it is already in its correct position.     
+      let isSwaped = false;
+      for (let j = 0; j < arr.length - i - 1; j++) { // this loops runs n-i-1 times the external loop
+        if (arr[j] > arr[j + 1]) { // if element at position j is greater than the element at position j + 1 swap both elements. For descending oreder jut replace > with <
+          let temp = arr[j] // temp variable holds the actual value of arr[j]
+          arr[j] = arr[j + 1];  // assigns the value of arr[j + 1] to arr
+          arr[j + 1] = temp; // assings the actual value of arr[j] to arr[j + 1]
+          isSwaped = true;
+        }
+      }
+      if (!isSwaped) { // already sorted array
+        break;
+      }
+    }
+    return arr;
+  }
+
+console.log(arr) // unsorted 
+bubbleSort(arr)
+console.log(arr) // sorted
+
+// the loop runs n * n the length of array.
+// if 5 items in an array the loop will run 5 * 5 = 25 times.
+// Time Complexity of bubble sort is O(n^2) wether it is optimized or not.
+// the outer loop runs n - 1 times as the largest/smallest number is already in the end after swapping so no need to loop over it.
+
---
+++ Algorithms/Sorting/selectionSort.js
@@ -0,0 +1,24 @@
+let arr = [64, 25, 12, 22, 11] 
+
+// ascending order
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length - 1; i++) { // b/c the last value will be smalles or largest depending on how you sort therefore no need to loop thill the end of the array.
+        // assume that the starting value of the unsorted part is always the samllest value.
+        let smallestIndex = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[smallestIndex]) { // if element at index j is smaller than the value at index smallestIndex then spaw them.
+                smallestIndex = j;
+            }
+        }
+        // swaping the element at smallestIndex with the element at index i.
+        [arr[i], arr[smallestIndex]] = [arr[smallestIndex], arr[i]];
+    }
+    return arr;
+}
+
+console.log("Unsorted array: ", arr);
+selectionSort(arr);
+console.log("Sorted array: ", arr);
+
+// Time complexity of selection sort is O(n^2) b/c of the double loops.
+// Space Complexity is O(1)
